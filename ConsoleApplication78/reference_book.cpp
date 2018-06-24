78be406e650ddad1f37e42aac44855646add06ca
takab <takab@LAPTOP-LTSQ7CE3>
2018-06-25 01:14:42 +0600

    Add project files.

---
+++ ConsoleApplication78/reference_book.cpp
@@ -0,0 +1,76 @@
+#include "stdafx.h"
+#include "reference_book.h"
+
+reference_book::reference_book()
+{
+	telephone_number = NULL;
+}
+
+void reference_book::SearchByName(string name_of_company)
+{
+	if (this->name_of_company == name_of_company)
+		cout << "We found this name of company: " << name_of_company << endl;
+	else
+		cout << "Sorry, we don't found this name of company, try later please. " << endl;
+}
+
+void reference_book::SearchByOwner(string owner)
+{
+	if (this->owner == owner)
+		cout << "We found this name of owner: " << owner << endl;
+	else
+		cout << "Sorry, we don't found this name of owner, try later please. " << endl;
+}
+
+void reference_book::SearchByTelephoneNumber(long long telephone_number)
+{
+	if (this->telephone_number == telephone_number)
+		cout << "We found owner with this telephone number:  " << telephone_number << endl;
+	else
+		cout << "Sorry, we don't found owner with this telephone number, try later please. " << endl;
+}
+
+void reference_book::SearchByHobby(string hobby)
+{
+	if (this->hobby == hobby)
+		cout << "We found owner by his hobby: " << hobby << endl;
+	else
+		cout << "Sorry, we don't found owner by entered hobby, try later. " << endl;
+}
+
+void reference_book::ShowReferenceBook()
+{
+	cout << "========== Reference book ==========" << endl;
+	cout << "Name of company: " << name_of_company << endl;
+	cout << "Owner of this company: " << owner << endl;
+	cout << "Telephone number: " << telephone_number << endl;
+	cout << "Adress: " << adress << endl;
+	cout << "Hobby: " << hobby << endl;
+	cout << "===== ===== ===== ===== ===== ===== " << endl << endl;
+}
+
+reference_book reference_book::Add(string name_of_company, string owner, long long telephone_number, string adress, string hobby)
+{
+	ofstream fout("Reference book.txt");
+
+	fout << "========== Reference book ========== " << endl;
+	fout << "Name of company: " << name_of_company << endl;
+	fout << "Owner: " << owner << endl;
+	fout << "Telephone number: " << telephone_number << endl;
+	fout << "Adress: " << adress << endl;
+	fout << "Hobby: " << hobby << endl;
+	fout << "===== ===== ===== ===== ===== ===== " << endl;
+
+	this->name_of_company = name_of_company;
+	this->owner = owner;
+	this->telephone_number = telephone_number;
+	this->adress = adress;
+	this->hobby = hobby;
+
+	fout.close();
+	return *this;
+}
+
+reference_book::~reference_book()
+{
+}
